19b58b69dd8aa58e4218d36f882e7bac46554a55
Catiusci Scheffer <cpcscheffer@outlook.com>
2022-11-22 00:12:18 -0300

    continuando estudando js

--- JavaScript_Developer_DIO/Projetos/POKEDEX/assets/js/main.js
+++ JavaScript_Developer_DIO/Projetos/POKEDEX/assets/js/main.js
@@ -1,9 +1,25 @@
-const limit = 10
-const offset = 0
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">${pokemon.number}</span>
+      <span class="name">${pokemon.name}</span>
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+      <div class="detail">
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join(' ')}
+        </ol>
+
+        <img src="${pokemon.photo}"
+             alt="${pokemon.name}">
+      </div>
+    </li>
+  `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newHtml = pokemons.map(convertPokemonToLi).join(' ')
+  pokemonList.innerHTML = newHtml
+})
